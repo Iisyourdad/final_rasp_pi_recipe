9506f04f2be37abef9daaa287a521c44f1b657a0
Iisyourdad <76657591+Iisyourdad@users.noreply.github.com>
2025-04-10 15:17:29 -0500

    second using the pycharm commit and push

--- push to git.py
+++ push to git.py
@@ -1,5 +1,4 @@
 import os
-
-os.system("git add .")
-os.system("git commit -m 'push to git'")
-os.system("git push origin master")
+os.system('git remote add origin https://github.com/Iisyourdad/final_rasp_pi_recipe.git')
+os.system('git branch -M main')
+os.system('git push -u origin main')
