6edaa0d427542234610472dafbdd806913dd9b1b
Iisyourdad <76657591+Iisyourdad@users.noreply.github.com>
2025-04-14 11:38:33 -0500

    hide curser

--- run_kiosk.sh
+++ run_kiosk.sh
@@ -5,8 +5,8 @@ unclutter -idle 0 -root &
 # Check if the configuration flag exists.
 if [ ! -f /home/tyler/configured.flag ]; then
     # If not configured, load the splash page.
-    /bin/chromium-browser --touch-events=enabled --enable-pinch --enable-touch-drag-drop --kiosk --ozone-platform=wayland --start-maximized http://127.0.0.1:8000/splash/
+    /bin/chromium-browser --touch-events=enabled --enable-pinch --enable-touch-drag-drop --kiosk --start-maximized http://127.0.0.1:8000/splash/
 else
     # Otherwise, launch the normal index page.
-    /bin/chromium-browser --touch-events=enabled --enable-pinch --enable-touch-drag-drop --kiosk --ozone-platform=wayland --start-maximized http://127.0.0.1:8000/
+    /bin/chromium-browser --touch-events=enabled --enable-pinch --enable-touch-drag-drop --kiosk --start-maximized http://127.0.0.1:8000/
 fi
